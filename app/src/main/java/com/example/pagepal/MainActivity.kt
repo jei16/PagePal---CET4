1a56ded98e43cc3d00bb1db2967180b2c2676c04
jc <jhoncarlodc41603@gmail.com>
2023-06-18 19:20:43 +0800

    splashscreen updated

--- app/src/main/java/com/example/pagepal/MainActivity.kt
+++ app/src/main/java/com/example/pagepal/MainActivity.kt
@@ -15,32 +15,6 @@ import com.example.pagepal.ui.theme.PagePalTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            PagePalTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
+        setContentView(R.layout.layout)
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PagePalTheme {
-        Greeting("Android")
-    }
 }
