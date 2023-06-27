2f7048e8dd71eff0fe1859cd05199db3dea85c99
unknown <kitkatfdalangin@gmail.com>
2023-06-27 23:29:28 +0800

    Second page of sign up screen updated. Login page and first page of sign up screen is also updated.

--- app/src/main/java/com/example/pagepal/getstarted.kt
+++ app/src/main/java/com/example/pagepal/getstarted.kt
@@ -2,10 +2,19 @@ package com.example.pagepal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+
 
 class getstarted : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_getstarted)
+
+        val loginButton: Button = findViewById(R.id.loginbutton)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, LoginScreenActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
