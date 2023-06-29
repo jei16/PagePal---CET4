711d777ea6f04024f3f2cadde0fc2692a0f740d1
unknown <kitkatfdalangin@gmail.com>
2023-06-29 13:44:24 +0800

    Sign Up text is now clickable and already directing to the Sign Up Screen + three screen pages of Forgot password are made

--- app/src/main/java/com/example/pagepal/LoginScreenActivity.kt
+++ app/src/main/java/com/example/pagepal/LoginScreenActivity.kt
@@ -4,10 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
 import android.widget.Button
+import android.widget.TextView
 
 class LoginScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loginscreen)
+
+        val signupScreen: TextView = findViewById(R.id.sign_up_text)
+        signupScreen.setOnClickListener {
+            val intent = Intent(this, SignUpScreenActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/pagepal/SignUpScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pagepal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signupscreen)
+    }
+}
