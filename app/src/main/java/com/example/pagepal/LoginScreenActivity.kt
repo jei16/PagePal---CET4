fd95d9e807d63d2c42a604dd3634f6f97d69ec85
John Carlo Alam <darkykrady12@gmail.com>
2023-07-24 08:01:57 +0800

    Fixed Email verification

--- app/src/main/java/com/example/pagepal/LoginScreenActivity.kt
+++ app/src/main/java/com/example/pagepal/LoginScreenActivity.kt
@@ -6,12 +6,21 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pagepal.databinding.ActivityLoginscreenBinding
+import com.example.pagepal.databinding.ActivityProfilePageBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class LoginScreenActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityLoginscreenBinding
+    private lateinit var binding: ActivityLoginscreenBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private var logAttempts = 0
+    private var logAttemptTime: Long = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginscreenBinding.inflate(layoutInflater)
@@ -32,33 +41,75 @@ class LoginScreenActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        binding.loginButton.setOnClickListener{
-            val username = binding.usernameEdittext.text.toString()
+        binding.loginButton.setOnClickListener {
+            val email = binding.usernameEdittext.text.toString()
             val password = binding.passwordEdittext.text.toString()
 
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                val logAttemptLimit = 3
+                val logAttemptWindow = 120000L
 
-            if (username.isNotEmpty() && password.isNotEmpty()){
-
-                    firebaseAuth.signInWithEmailAndPassword(username, password).addOnCompleteListener {
-                        if(it.isSuccessful) {
-                            val intent = Intent(this,MainActivity::class.java)
-                            startActivity(intent)
+                getUserIDFromEmail(email) { userID ->
+                    if (userID != null){
+                        val currentTime = System.currentTimeMillis()
+                        if (logAttempts < logAttemptLimit || currentTime - logAttemptTime >= logAttemptWindow){
+                            firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener {
+                                if (it.isSuccessful){
+                                    val Verify = firebaseAuth.currentUser?.isEmailVerified
+                                    if (Verify == true){
+                                        val intent = Intent(this, ActivityProfilePageBinding::class.java)
+                                        startActivity(intent)
+                                    }else{
+                                        Toast.makeText(this, "Please Verify your Email", Toast.LENGTH_SHORT).show()
+                                    }
+                                }else{
+                                    Toast.makeText(this, "Login Failed, Please check your password.", Toast.LENGTH_SHORT).show()
+                                    logAttempts++
+                                    logAttemptTime = currentTime
+                                }
+                            }
                         }else{
-                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                            Toast.makeText(this, "Try Again in 2 minutes.", Toast.LENGTH_SHORT).show()
                         }
+                    }else{
+                        Toast.makeText(this, "Email is not found", Toast.LENGTH_SHORT).show()
                     }
-            }else{
+                }
+            } else {
                 Toast.makeText(this, "Please fill in the blanks", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-   override fun onStart() {
-     super.onStart()
+    override fun onStart() {
+        super.onStart()
 
-       if(firebaseAuth.currentUser !=null){
-           val intent = Intent(this, MainActivity::class.java)
-           startActivity(intent)
-       }
+        if (firebaseAuth.currentUser != null) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
-}
+
+    private fun getUserIDFromEmail(email: String, callback: (String?) -> Unit) {
+            val db = FirebaseDatabase.getInstance().reference
+            val URef = db.child("users")
+
+            URef.orderByChild("email").equalTo(email)
+                .addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        if (snapshot.exists()) {
+                            for (userSnapshot in snapshot.children) {
+                                val userID = userSnapshot.key
+                                callback(userID)
+                                return
+                            }
+                        }
+                        callback(null)
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        callback(null)
+                    }
+                })
+            }
+    }
--- app/src/main/java/com/example/pagepal/SignUpScreenActivity.kt
+++ app/src/main/java/com/example/pagepal/SignUpScreenActivity.kt
@@ -1,46 +1,58 @@
 package com.example.pagepal
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import android.widget.Button
 import android.widget.Toast
-import com.example.pagepal.databinding.ActivitySignupscreenBinding
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pagepal.databinding.ActivitySignupscreen3Binding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class SignUpScreenActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySignupscreenBinding
+    private lateinit var binding: ActivitySignupscreen3Binding
     private lateinit var firebaseAuth: FirebaseAuth
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivitySignupscreenBinding.inflate(layoutInflater)
+        binding = ActivitySignupscreen3Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
         firebaseAuth = FirebaseAuth.getInstance()
 
-        binding.signupBtn.setOnClickListener {
-            val name = binding.nameEditText.text.toString()
-            val email = binding.emailEditText.text.toString()
+        binding.btnSignup3.setOnClickListener {
+            val name = binding.EditTxtUsername.text.toString()
+            val email = binding.EditTxtEmail.text.toString()
+            val pass = binding.EditTxtPass.text.toString()
+            val cpass = binding.EditTxtCPass.text.toString()
 
-            if (name.isNotEmpty() && email.isNotEmpty()) {
-                firebaseAuth.createUserWithEmailAndPassword(email, name)
+            if (email.isNotEmpty() && pass.isNotEmpty() && cpass.isNotEmpty()) {
+                if (pass == cpass){
+                    firebaseAuth.createUserWithEmailAndPassword(email, pass)
                     .addOnCompleteListener { task ->
                         if (task.isSuccessful) {
-                            val intent = Intent(this, LoginScreenActivity::class.java)
-                            startActivity(intent)
-                        } else {
-                            Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT)
+
+                            val user = firebaseAuth.currentUser
+                            val URef = FirebaseDatabase.getInstance().getReference("users")
+                            URef.child(user?.uid?:"").child("name").setValue(name)
+
+                            firebaseAuth.currentUser?.sendEmailVerification()?.addOnCompleteListener{
+                                Toast.makeText(this, "Please Verify your Email.", Toast.LENGTH_SHORT).show()
+                            }?.addOnFailureListener{
+                                Toast.makeText( this, task.toString(), Toast.LENGTH_SHORT).show()
+                            }
+                                } else {
+                                    Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT)
                                 .show()
-                        }
+                                }
+                            }
+                    } else {
+                Toast.makeText(this, "Passwords do not match.", Toast.LENGTH_SHORT).show()
                     }
-            } else {
-                Toast.makeText(this, "Please fill in the blanks", Toast.LENGTH_SHORT).show()
-            }
+             }else{
+                Toast.makeText(this, "Please fill in the blanks.", Toast.LENGTH_SHORT).show()
+             }
         }
 
     }
