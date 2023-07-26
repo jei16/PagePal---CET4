e50d04f4448c675072c1fb04cc39b2c2be93c53f
unknown <urielryanreyles@gmail.com>
2023-07-26 17:56:26 +0800

    book preview on click layout done, and updated splashscreen duration

---
+++ app/src/main/java/com/example/pagepal/bookPreview.kt
@@ -0,0 +1,11 @@
+package com.example.pagepal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class bookPreview : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_preview)
+    }
+}
