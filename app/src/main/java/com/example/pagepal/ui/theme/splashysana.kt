83109dcf210b0526f0a71a1560bb70c76b0d05ec
unknown <urielryanreyles@gmail.com>
2023-07-26 10:59:44 +0800

    designed a new splashscreen but not applied, and fixed some design errors

---
+++ app/src/main/java/com/example/pagepal/ui/theme/splashysana.kt
@@ -0,0 +1,12 @@
+package com.example.pagepal.ui.theme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pagepal.R
+
+class splashysana : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashysana)
+    }
+}
